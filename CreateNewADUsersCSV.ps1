c5d8a72f2262b083dfda6db3a41ce90a47f0ac2a
Mark Browning <marksbrowning3@gmail.com>
2020-03-06 10:30:24 -0500

    Added section to set AD password.
    Signed-off-by: Mark Browning <marksbrowning3@gmail.com>

--- CreateNewADUsersCSV.ps1
+++ CreateNewADUsersCSV.ps1
@@ -7,44 +7,48 @@ $_ = import-csv -Path "\\server\share\folder\NewUsers.csv" | ForEach-Object
 
         #Define Attributes from CSV 
             
-            $GivenName = $_.FirstName
-            $Middle = $_.Middle
-            $SurName = $_.LastName
-            $Division = $_.Division
-            $Department = $_.Department
-            $JobTitle = $_.JobTitle
-            #$Manager = $_.Manager
-            $Phone = $_.Phone
-            #$Name = $SurName + ", " + $GivenName + ", " + $Middle
-            $DisplayName = $GivenName + " " + $SurName
-            $SAM = $GivenName[0] + $Middle + $SurName
-            $Email = $SAM + '@winstonind.com'
-            $Proxy = 'SMTP:' + $Email
-            $Password = ConvertTo-SecureString -AsPlainText -String "P4ssword" -Force
-            $Office = 'Contoso ' + " " + $Division
-            $Company = "Contoso"
-            $ScriptPath = "script.bat"
-            $OU = $_.OU
+        $GivenName = $_.FirstName
+        $Middle = $_.Middle
+        $SurName = $_.LastName
+        $Division = $_.Division
+        $Department = $_.Department
+        $JobTitle = $_.JobTitle
+        #$Manager = $_.Manager
+        $Phone = $_.Phone
+        #$Name = $SurName + ", " + $GivenName + ", " + $Middle
+        $DisplayName = $GivenName + " " + $SurName
+        $SAM = $GivenName[0] + $Middle + $SurName
+        $Email = $SAM + '@winstonind.com'
+        $Proxy = 'SMTP:' + $Email
+        $Password = ConvertTo-SecureString -AsPlainText -String "P4ssword" -Force
+        $Office = 'Contoso ' + " " + $Division
+        $Company = "Contoso"
+        $ScriptPath = "script.bat"
+        $OU = $_.OU
 
         #Create New Users
 
-            New-ADUser `
-            -Name "$DisplayName" `
-            -AccountPassword $Password `
-            -Department "$Department" `
-            -Enabled $true `
-            -GivenName "$GivenName" `
-            -Initials "$Middle" `
-            -OtherAttributes @{'ProxyAddresses' = "$Proxy"} `
-            -SamAccountName "$SAM" `
-            -DisplayName "$DisplayName" `
-            -Surname "$SurName" `
-            -ScriptPath "$ScriptPath" `
-            -Path "$OU" `
-            -UserPrincipalName "$Email" `
-            -Office "$Office" `
-            -Company "$Company" `
-            -Title "$JobTitle" `
-            -EmailAddress "$Email" `
-            -OfficePhone "$Phone"
+        New-ADUser `
+        -Name "$DisplayName" `
+        -AccountPassword $Password `
+        -Department "$Department" `
+        -Enabled $true `
+        -GivenName "$GivenName" `
+        -Initials "$Middle" `
+        -OtherAttributes @{'ProxyAddresses' = "$Proxy"} `
+        -SamAccountName "$SAM" `
+        -DisplayName "$DisplayName" `
+        -Surname "$SurName" `
+        -ScriptPath "$ScriptPath" `
+        -Path "$OU" `
+        -UserPrincipalName "$Email" `
+        -Office "$Office" `
+        -Company "$Company" `
+        -Title "$JobTitle" `
+        -EmailAddress "$Email" `
+        -OfficePhone "$Phone"
+
+        # Set new user's password
+        
+        Set-ADAccountPassword -Identity $SAM -NewPassword $Password -Reset
 }
